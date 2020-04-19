6c8ff92538710cf3415c89fc3e0a8664b5059f30
FABIO MULLER <fabioml10@yahoo.com.br>
2020-04-19 00:19:52 -0300

    Menu created

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -2,14 +2,16 @@ import React, { Fragment } from 'react';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
+import Menu from './components/common/menu'
 
-import 'app.scss';
+import '../javascript/app.scss';
  
  
 const App = () => {
   return(
     <Fragment>
       <BrowserRouter>
+        <Menu/>
         <Routes/>
       </BrowserRouter>
     </Fragment>
