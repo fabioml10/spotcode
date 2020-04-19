5e2a71bad578786db3904739ba450f3acd885218
FABIO MULLER <fabioml10@yahoo.com.br>
2020-04-18 22:50:52 -0300

    Api favorites endpoint created

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
   root "home#index"
   get 'home/index'
 
+  #rotas reaproveitaveis, passa para o resourses songs como parametro
+  concern :favoritable do |options|
+    shallow do
+      post "/favorite", { to: "favorites#create", on: :member }.merge(options)
+      delete "/favorite", { to: "favorites#destroy", on: :member }.merge(options)
+    end
+  end
+
   #especifico para apis
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
@@ -14,6 +22,12 @@ Rails.application.routes.draw do
       resources :albums, only: :show do
         #rota: /api/v1/albums/:id/recently_heards, ou seja, fica aninhada ao album
         resources :recently_heards, only: :create
+      end
+
+      resources :favorites, only: :index
+
+      resources :songs, only: [] do
+        concerns :favoritable, favoritable_type: 'Song'
       end
 
     end
