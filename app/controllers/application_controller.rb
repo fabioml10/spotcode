5354a454bd537cc9482bf68f801a92f3d26c477b
FABIO MULLER <fabioml10@yahoo.com.br>
2020-04-18 21:52:43 -0300

    Api albums endpoint created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
   #require authentication on all pages
   before_action :authenticate_user!
+
+  #libera chamadas de fora do app quando for json (post no ajax?)
+  protect_from_forgery unless: -> { request.format.json? }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,16 @@ Rails.application.routes.draw do
   #especifico para apis
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+
       resources :dashboard, only: :index
       resources :categories, only: %i[index show]
       resources :search, only: :index
+
+      resources :albums, only: :show do
+        #rota: /api/v1/albums/:id/recently_heards, ou seja, fica aninhada ao album
+        resources :recently_heards, only: :create
+      end
+
     end
   end
 
