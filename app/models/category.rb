09f3147a81069e86052cbfb369f0f8ac6a28137a
FABIO MULLER <fabioml10@yahoo.com.br>
2020-04-18 21:28:54 -0300

    Api categories endpoint created

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,14 @@ class Category < ApplicationRecord
   has_many :albums
   validates :name, presence: true
   has_one_attached :image
+
+  def artists
+    #self é a categoria mesmo
+    Artist.joins(:albums).where(albums: { id: self.albums.ids }).distinct
+  end
+
+  def songs
+    Song.joins(:album).where(songs: { id: self.albums.ids }).distinct
+  end
+
 end
