649aa629f13c1ebf451bc66b915a2857956c1042
FABIO MULLER <fabioml10@yahoo.com.br>
2020-04-18 23:20:06 -0300

    Bulma installed

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,10 +1,13 @@
 import React, { Fragment } from 'react';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Button } from 'react-bulma-components';
  
 const App = () => {
   return(
     //fragmente envelopa os codigos
     <Fragment>
-      Hello App!
+      Hello App!<br></br>
+      <Button color="primary">Testando o Bulma</Button>
     </Fragment>
   );
 }
