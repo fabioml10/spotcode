415270dfe6fd4f8875739fc395e2448014ad730c
FABIO MULLER <fabioml10@yahoo.com.br>
2020-04-18 23:24:16 -0300

    Scss imported into App.js

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,10 +1,11 @@
 import React, { Fragment } from 'react';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import { Button } from 'react-bulma-components';
+import 'app.scss';
  
 const App = () => {
   return(
-    //fragmente envelopa os codigos
+    //fragment envelopa os codigos
     <Fragment>
       Hello App!<br></br>
       <Button color="primary">Testando o Bulma</Button>
