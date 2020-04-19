8805f0e9ca308a51ce6393ebd491232873b0ef9b
FABIO MULLER <fabioml10@yahoo.com.br>
2020-04-18 23:13:12 -0300

    JS files for React created

---
+++ app/javascript/app.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+ 
+const App = () => {
+  return(
+    //fragmente envelopa os codigos
+    <Fragment>
+      Hello App!
+    </Fragment>
+  );
+}
+
+export default App;
